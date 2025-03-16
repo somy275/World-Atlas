f7777acd1e8083798d157fcfa875524163cfd919
somy275 <somyranjank@gmail.com>
2025-03-16 19:08:04 +0530

    Some changes in pagination

--- src/UI/CountryPagination.jsx
+++ src/UI/CountryPagination.jsx
@@ -1,14 +1,43 @@
-/* eslint-disable react/prop-types */
 import { Pagination } from "@heroui/react";
+/* eslint-disable react/prop-types */
 export const CountryPagination = ({ totalPages, setCurrentPage }) => {
     let onPage = (e) => {
         setCurrentPage(e)
         console.log(e);
     }
-
     return (
-        <Pagination size={window.innerWidth<600?"sm":"lg"} classNames={{ item: " bg-[#27272A] font-bold  text-white rounded-full cursor-pointer", cursor: "bg-[#ff8531] rounded-full text-white font-bold" }} showControls boundaries={window.innerWidth < 600 ? 1 : 3} color="secondary" initialPage={1} total={totalPages}
-            onChange={onPage}
-        />
+
+        <>
+            <Pagination classNames={{ wrapper: "flex items-center justify-around gap-3 ", item: ` rounded-full bg-[#27272A] data-[active=true]:bg-[#ff8531] hover:bg-[#3F3F46] text-white font-bold cursor-pointer ${window.innerWidth < 650 ? "h-[1.8rem] w-[1.8rem]" : " h-[2.4rem] w-[2.4rem]"}`, prev: "cursor-pointer active:scale-[0.85] active:transition-transform active:duration-[200ms] data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50", next: "cursor-pointer active:scale-[0.85] active:transition-transform active:duration-[200ms] data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50", ellipsis: "cursor-pointer  group-hover:hidden group-data-[focus-visible=true]:block data-[before=true]:rotate-180", base: "", cursor: "" }} showControls boundaries={window.innerWidth < 650 ? 1 : 3} initialPage={1} total={totalPages}
+                onChange={onPage}
+            />
+
+        </>
+
+
+
+
     )
 }
+export const ChevronIcon = (props) => {
+    return (
+        <svg
+            aria-hidden="true"
+            fill="none"
+            focusable="false"
+            height="1em"
+            role="presentation"
+            viewBox="0 0 24 24"
+            width="1em"
+            {...props}
+        >
+            <path
+                d="M15.5 19l-7-7 7-7"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+            />
+        </svg>
+    );
+};
